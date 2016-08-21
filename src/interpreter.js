230a840f601bd0e942d7da17fd4cadd8d7f29e41
Kevin Wang <wangkevin448@gmail.com>
2016-08-20 23:02:20 -0700

    interpreter

--- src/interpreter.js
+++ src/interpreter.js
@@ -1,17 +1,34 @@
 import React from 'react';
+import Remarkable from 'remarkable';
 
 const h = React.createElement;
+const md = new Remarkable('commonmark');
 
 const interpreter = (bulletjson, views)=>{
   if(typeof bulletjson === 'string'){
-    // use remarkable to output html
+    return h('div', {
+      // explicitly trust remarkable, nothing else
+      dangerouslySetInnerHTML: {
+        __html: md.render()
+      }
+    });
   }
 
   if(!Array.isArray(bulletjson)){
     // interpret component
     const {component, props, children} = bulletjson;
     if(views[component]){
-      return h(views[component], props, interpreter(children, views));
+      if(props){
+        if(children){
+          return h(views[component], props, interpreter(children, views));
+        } else {
+          return h(views[component], props);
+        }
+      } else {
+        if(children){
+          return h(views[component], null, interpreter(children, views));
+        }
+      }
     } else {
       return '';
     }
