63d472beb2f2afdaab95f808cadf5222bdff65b5
Kevin Wang <wangkevin448@gmail.com>
2016-08-20 22:42:19 -0700

    half of interpreter

--- src/interpreter.js
+++ src/interpreter.js
@@ -2,8 +2,27 @@ import React from 'react';
 
 const h = React.createElement;
 
-const interpreter = (bulletjson)=>{
-  
+const interpreter = (bulletjson, views)=>{
+  if(typeof bulletjson === 'string'){
+    // use remarkable to output html
+  }
+
+  if(!Array.isArray(bulletjson)){
+    // interpret component
+    const {component, props, children} = bulletjson;
+    if(views[component]){
+      return h(views[component], props, interpreter(children, views));
+    } else {
+      return '';
+    }
+  }
+
+  // interpret array
+  const viewjson = [];
+  for(let i of bulletjson){
+    viewjson.push(interpreter(i, views));
+  }
+  return viewjson;
 };
 
 export {interpreter};
